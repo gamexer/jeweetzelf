7cddcb6f261a9138c01a316c7b813bae7bcc08d3
DikkeNeef <hans.gillis@ucb.com>
2017-10-16 19:26:23 +0200

    q
    q

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from os import getcwd
 from time import sleep
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-driver = Chrome('chromedriver', chrome_options=chrome_options)
+driver = webdriver.PhantomJS()
 driver.get("http://chirojezuseik.be/")
 while True:
     sleep(100)
